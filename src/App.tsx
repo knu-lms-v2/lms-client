76cf6f34fba149ac98acac80a5b0a0672146dd2d
TaehyunJeon <jeontaehyun0203@gmail.com>
2025-07-02 17:18:57 +0900

    Fix: CSS 파일 타입 선언

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
-import React from 'react';
-import Sidetab from './components/layout/Sidetab';
-import './App.css';
+import React from "react";
+import Sidetab from "./components/layout/Sidetab";
+import "./App.css";
 
 function App() {
   return (
     <div className="app-layout">
       <Sidetab />
       <main className="main-content">
-        {/* 메인 콘텐츠가 이 곳에 표시됩니다. */}
         <h1>메인 콘텐츠 영역</h1>
-        <p>페이지의 나머지 내용이 여기에 들어갑니다.</p>
       </main>
     </div>
   );
