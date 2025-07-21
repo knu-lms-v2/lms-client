679790b40193a38cd95595cf94822990bacab64b
TaehyunJeon <jeontaehyun0203@gmail.com>
2025-07-22 01:04:45 +0900

    Feat: 캘린터 테스트 이벤트 추가

--- src/components/common/Calender.tsx
+++ src/components/common/Calender.tsx
@@ -7,9 +7,19 @@ type CalenderProps = {
 };
 
 const Calender = ({ className }: CalenderProps) => {
+  const events = [
+    { title: "event1", date: "2025-07-22" },
+    { title: "event2", date: "2025-07-23" },
+    { title: "event3", date: "2025-07-24" },
+  ];
+
   return (
     <div className={`${styles.calenderContainer} ${className || ""}`}>
-      <FullCalendar plugins={[dayGridPlugin]} initialView="dayGridMonth" />
+      <FullCalendar
+        plugins={[dayGridPlugin]}
+        initialView="dayGridMonth"
+        events={events}
+      />
     </div>
   );
 };
